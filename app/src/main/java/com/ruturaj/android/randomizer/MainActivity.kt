bd6d5576557bd012386bf34bffe85792f97ec451
Ruturaj <ruturajkvaidya@gmail.com>
2018-11-26 20:42:42 -0600

    Initial commit

---
+++ app/src/main/java/com/ruturaj/android/randomizer/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.ruturaj.android.randomizer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        resultsTextView.text = "0"
+
+        rollButton.setOnClickListener{
+            val rand = Random().nextInt(seekBar.progress) + 2
+            resultsTextView.text = rand.toString()
+        }
+    }
+}
